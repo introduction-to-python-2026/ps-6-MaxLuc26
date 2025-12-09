c60926d18a03e4b4bac88247acbfdae6ba23e2e1
MaxLuc26 <maxlucasberman@gmail.com>
2025-12-09 17:34:59 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    amino_dict = {}
+    for i in rows:
+      stripped = i.strip()
+      polished = stripped.split('\t')
+      amino_dict[polished[0]] = polished[2]
+    return amino_dict
 
 
